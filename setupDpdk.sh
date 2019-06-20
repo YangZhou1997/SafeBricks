a6580955c7cc5f00b61d4f5c1af0099478ce14db
yangzhou <yangzhou@node-0.yangzhou-qv53847.lambda-mpi-pg0.clemson.cloudlab.us>
2019-06-20 10:36:04 -0600

    add dpdk change support:

--- setupDpdk.sh
+++ setupDpdk.sh
@@ -7,14 +7,18 @@
 #sudo bash ../utils/vm-setup.sh
 
 DPDK_HOME=~/tools/dpdk-stable-17.08.1
-CFLAGS="-g3 -Wno-error=maybe-uninitialized -fPIC"
+
+# define HG_MON if you want dpdk to print memzone info.
+# Canno pass HG_MON to the dpdk code ?? 
+CFLAGS="-g3 -Wno-error=maybe-uninitialized -fPIC -DHG_MON=1"
 
 sudo apt-get -y install build-essential ca-certificates curl \
     libnuma-dev libpcap-dev xz-utils
 
 cd ~/tools
-git clone git@github.com:YangZhou1997/dpdk-stable-17.08.1.git
-# curl -sSf https://fast.dpdk.org/rel/dpdk-17.08.1.tar.xz | tar -xJv
+[ ! -d "dpdk-stable-17.08.1" ] && git clone git@github.com:YangZhou1997/dpdk-stable-17.08.1.git
+    # curl -sSf https://fast.dpdk.org/rel/dpdk-17.08.1.tar.xz | tar -xJv
+
 cp ~/utils/dpdk/common_linuxapp-17.08 $DPDK_HOME/config/common_linuxapp
 
 cd $DPDK_HOME
@@ -26,3 +30,7 @@ sudo make install
 sudo insmod $DPDK_HOME/build/kmod/igb_uio.ko
 
 sudo $DPDK_HOME/usertools/dpdk-devbind.py --force -b igb_uio 0000:81:00.1
+
+bash ~/NetBricks/setupDpdkCopy.sh
+
+echo "please rebuild NetBricks to make dpdk changes valid"
--- setupDpdkCopy.sh
+++ setupDpdkCopy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 sudo mkdir -p /opt/dpdk/build
+sudo mkdir -p /opt/dpdk/build/include
+sudo mkdir -p /opt/dpdk/build/lib
+sudo mkdir -p /opt/dpdk/build/include/exec-env
+sudo mkdir -p /opt/dpdk/build/include/generic
+
+sudo rm -rf ~/trash/*
 
 src=~/tools/dpdk-stable-17.08.1/build/lib
 dst=/opt/dpdk/build/lib
@@ -8,10 +14,10 @@ dst=/opt/dpdk/build/lib
 #clean destination
 if [ -d $dst ]; then
    echo "Cleaning target directory $dst"
-   sudo rm -rf $dst/*
+   sudo mv $dst/* ~/trash/
 fi
 
-sudo cp -r $src $dst
+sudo cp -r $src/* $dst
 
 src=~/tools/dpdk-stable-17.08.1/build/include
 dst=/opt/dpdk/build/include
@@ -19,7 +25,7 @@ dst=/opt/dpdk/build/include
 #clean destination
 if [ -d $dst ]; then
    echo "Cleaning target directory $dst"
-   sudo rm -rf $dst/*
+   sudo mv $dst/* ~/trash/
 fi
 
 #copy files
@@ -42,7 +48,7 @@ dst=/opt/dpdk/build/include/exec-env
 #clean destination
 if [ -d $dst ]; then
    echo "Cleaning target directory $dst"
-   sudo rm -rf $dst/*
+   sudo mv $dst/* ~/trash/
 fi
 
 #copy files
@@ -64,7 +70,7 @@ dst=/opt/dpdk/build/include/generic
 #clean destination
 if [ -d $dst ]; then
    echo "Cleaning target directory $dst"
-   sudo rm -rf $dst/*
+   sudo mv $dst/* ~/trash/
 fi
 
 #copy files
